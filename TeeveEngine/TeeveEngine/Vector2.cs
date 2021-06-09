f139ea11c1c8d3f8a52b3c48bdf2c33f62aa5365
tcrurup <49696152+tcrurup@users.noreply.github.com>
2021-06-09 15:28:02 -0600

    Add References and create Vector2 class

---
+++ TeeveEngine/TeeveEngine/Vector2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeeveEngine.TeeveEngine {
+    public class Vector2 {
+        public float Y { get; set; }
+        public float X { get; set; }
+
+
+        public Vector2() {
+            X = Zero().X;
+            Y = Zero().Y;
+        }
+
+        public Vector2(float x, float y) {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2 Zero() {
+            return new Vector2(0, 0);
+        }
+    }
+}
